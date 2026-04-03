9729a400abd2b475e7d6376b5292821f0e32ffe9
Mihir Singh Bisht <mihir42002@gmail.com>
2026-04-03 21:36:17 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/financial_backend/FinancialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.financial_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financial_backend/FinancialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
